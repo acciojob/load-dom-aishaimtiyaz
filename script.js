1bb9f088ae1619a2a613278044150391e85d68a7
aishaimtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2023-12-14 22:13:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-onDOMLoad(){
-	document.body.textContent="DOM load success";
-	
-}
-document.addEventListner("DomContentLoaded",onDOMLoad);
+//your JS code here. If required.
+ document.addEventListener("DOMContentLoaded", function() {
+            var body = document.querySelector("body");
+            body.innerText = "DOM load success";
+        });
